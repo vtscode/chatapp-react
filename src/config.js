b42666d0f9090dad9c18fec23aa5051b2f110d4f
rhivent <zzzzjrjr@gmail.com>
2019-10-29 14:10:02 +0700

    commit awal chat room

---
+++ src/config.js
@@ -0,0 +1,8 @@
+/** swap out */
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/cc94a935-bf7d-4412-bbb7-a5ba0fc08693/token";
+// https://us1.pusherplatform.io/services/chatkit_token_provider/v1/:instance_id
+
+/** swap out */
+const instanceLocator = "v1:us1:cc94a935-bf7d-4412-bbb7-a5ba0fc08693";
+
+export { tokenUrl, instanceLocator }
